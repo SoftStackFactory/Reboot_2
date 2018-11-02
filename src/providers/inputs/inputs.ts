d429d12b60d2ee9c2d9e94a19f38ba87c41d9b6b
BrettWray <brett@brettwray.com>
2018-11-02 15:51:22 -0700

    set the Status array on sliders.ts to be a behavior subject so the chart component can subscribe to the value

--- src/providers/inputs/inputs.ts
+++ src/providers/inputs/inputs.ts
@@ -3,16 +3,20 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class InputsProvider {
-
+  //Status Source is the RXJS behavior subject, this allows it to update as the component is updated.
   private statusSource = new BehaviorSubject([]);
+  //Current status is the observable.
   currentStatus = this.statusSource.asObservable();
 
   constructor() {
   }
-
+  //Function that is called on sliders.ts, changes the value of the section in real time.
   changeStatus(val : any){
     this.statusSource.next(val)
+    console.log('inputs',val)
   }
+  //get status can be subscribed to in the chart component so that as the sliders update the
+  //chart can be updated.
   getStatus(){
     
   }
