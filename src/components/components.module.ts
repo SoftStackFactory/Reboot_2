fc186d97817d3c1e1d3f21918d7f5ec57508b34b
BrettWray <brett@brettwray.com>
2018-11-03 20:37:29 -0700

    small changes to accomodate for the chart component being added

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { SlidersComponent } from './sliders/sliders';
 import { CommonModule } from '@angular/common';
 import { ChartComponent } from './chart/chart';
+
 @NgModule({
 	declarations: [SlidersComponent,
     ChartComponent],
 	imports: [ CommonModule, IonicModule],
 	exports: [SlidersComponent,
-    ChartComponent]
+    ChartComponent],
 })
 export class ComponentsModule {}
