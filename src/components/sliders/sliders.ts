d4150131708536c6d3534a941d9eff068bd5fea3
BrettWray <brett@brettwray.com>
2018-11-03 20:38:15 -0700

    small update for chart component

--- src/components/sliders/sliders.ts
+++ src/components/sliders/sliders.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputsProvider } from './../../providers/inputs/inputs';
 
 @Component({
   selector: 'sliders',
   templateUrl: 'sliders.html'
 })
-export class SlidersComponent {
+export class SlidersComponent implements OnInit {
   
  //Initializes the different values that users can rate
   career: number = 5;
@@ -25,7 +25,7 @@ export class SlidersComponent {
   change(){
     //Status is an array of the 8 values, Charts.Js uses in array to
     //build the chart
-    let Status = [
+    let statusChange = [
       this.attitude,
       this.career,
       this.family,
@@ -37,11 +37,21 @@ export class SlidersComponent {
                 ]
     //sends the status array to the inputs service where it sets the value
     //of the behavior subject so that it can update in realtime as an observable
-    this.Inputs.changeStatus(Status)
+    this.Inputs.changeStatus(statusChange)
 
   }
-  ionViewDidLoad(){
-
+  ngOnInit(){
+    let initStatus = [
+      this.attitude,
+      this.career,
+      this.family,
+      this.finance,
+      this.health,
+      this.family,
+      this.relationships,
+      this.socialLife
+                ]
+    this.Inputs.changeStatus(initStatus)
   }
  
 
