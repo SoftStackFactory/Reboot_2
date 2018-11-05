74a99cb0d6ad1c320a75a66dc1d290535ebf878a
BrettWray <brett@brettwray.com>
2018-11-04 16:08:54 -0800

    added npm chjarts js plugin labels package

--- src/components/chart/chart.ts
+++ src/components/chart/chart.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { InputsProvider } from './../../providers/inputs/inputs';
 import { Chart } from 'chart.js'
 import 'chartjs-plugin-datalabels';
+import 'chartjs-plugin-labels';
 
 @Component({
   selector: 'chart',
@@ -33,12 +34,12 @@ export class ChartComponent implements OnInit {
         ]
       },
       options: {
-
+        
         layout: {
           padding: {
             top: 5,
             bottom: 5
-          }
+          },
         },
 
         legend: {
@@ -46,33 +47,15 @@ export class ChartComponent implements OnInit {
         },
 
         plugins: {
-          datalabels: {
-            textAlign: 'center',
-            anchor: 'start',
-            align: 'end',
-            offset: function (context) {
-              let chartWidth = context.chart.width;
-              return chartWidth / 4.0 - 60;
+          labels: [
+            {
+              render: 'label',
+              fontColor: '#000',
+              fontSize: 14,
+              position: 'outside',
+              arc: true
             },
-            backgroundColor: ["rgba(0,0,255, .2)", "rgba(255,0,0, .2)", "rgba(128,0,128, .2)", "rgba(0,128,0, .2)", "rgba(255,165,0, .2)", "rgba(0,128,128, .2)", "rgba(255,0,255, .2)", "rgba(0,255,0, .2)"],
-            borderColor: 'black',
-            rotation: function (context) {
-              if (context.dataIndex === 0 || context.dataIndex === 1 || context.dataIndex === 6 || context.dataIndex === 7) { return 45 / 2 + (45 * context.dataIndex) }
-              else { return 45 / 2 + (45 * context.dataIndex) + 180 }
-            },
-            borderRadius: 5,
-            borderWidth: 0,
-            color: 'black',
-            font: function (context) {
-              var width = context.chart.width;
-              var size = Math.round(width / 60);
-              return {
-                size: size,
-                weight: 600,
-                font: 'Lato'
-              };
-            },
-          }
+          ]
         }
       }
     });
@@ -84,11 +67,12 @@ export class ChartComponent implements OnInit {
     Chart.defaults.polarArea.animation.animateScale = false;
     Chart.defaults.polarArea.scale.ticks.min=0;
     Chart.defaults.polarArea.scale.ticks.max=10;
-
+    
   }
   ngAfterViewInit(){
     
     this.status = this.input.currentStatus.subscribe((res)=>{
+      console.log(res)
       this.data = res;
       this.updateChart(res)
     })
